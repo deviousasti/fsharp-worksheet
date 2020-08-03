a0062e65f3b6c7cf0e6508583459ee2b3a623085
asti <asti@dynz.net>
2020-08-03 12:17:18 +0530

    Add DTOs

---
+++ src/DT.fs
@@ -0,0 +1,25 @@
+﻿namespace FsWorksheet
+
+open System
+
+type vspos = { Line: int; Col: int }
+type vsrange = { From: vspos; To: vspos }
+type Run = ConsoleColor * string
+type Runs = Run []
+type eqhash = int32
+type vscell = { hash: eqhash; range: vsrange }
+
+type WorksheetCommand =
+| Compute of source: string
+| ForceEvalCellAt of vspos
+| ForceEvalRange of vsrange
+| Interrupt
+| Noop
+| Exit
+
+
+type WorksheetEvent =
+| Query
+| PreEval of vsrange * eqhash
+| PostEval of vsrange * eqhash * Runs 
+| CellsChanged of vscell []
