1c6c06caebf32a0d4a47cbd27233df7e2acb6f00
asti <asti@dynz.net>
2020-08-10 21:23:31 +0530

    Refactor to use while instead of async loop - might throw out because Rpc is fighting with VS's threading model

--- server/ServerProgram.fs
+++ server/ServerProgram.fs
@@ -5,58 +5,57 @@ open FsWorksheet.API
 open FsWatch
 open FSharp.Control.Reactive
 open FSharp.Compiler
+open System.Threading
 
 [<EntryPoint>]
 let main argv =
     let name, file = argv.[0], argv.[1]
-    use client = Rpc.createClient name 2000
+    use client = Rpc.createClient name 200
     use subject = Subject.broadcast
 
-    let post message = 
+    let sendReceive message = 
         lock client <| fun () -> 
         Async.RunSynchronously <| async { 
-        let! _ = client.post message 
-        return ()
+        return! client.post message 
     }
+
+    let send = sendReceive >> ignore
 
     let config = { 
         filename = file
         events = {
             onBeforeEval = fun cell -> 
-                post <| PreEval ({ range = Range.toVsRange cell.range; eqHash = cell.eqHash }, cell.ToSource()) 
+                send <| PreEval ({ range = Range.toVsRange cell.range; eqHash = cell.eqHash }, cell.ToSource()) 
             onAfterEval = fun cell -> 
-                post <| PostEval ({ range = Range.toVsRange cell.range; eqHash = cell.eqHash }, cell.runs)
+                send <| PostEval ({ range = Range.toVsRange cell.range; eqHash = cell.eqHash }, cell.runs)
             onStaging = fun (state) -> 
-                post <| ChangesStaged (
+                send <| ChangesStaged (
                     state.cells 
                     |> Seq.map(fun cell -> { eqHash = cell.eqHash; range = Range.toVsRange cell.range })
                     |> Seq.toArray
                 )
             onCommit = fun _ ->
-                post <| Committed
+                send <| Committed
         }
     }
 
     use watch = FsWatch.watch (config, subject)
+    printfn "Server started for %s" file
 
-    let rec loop next = async {
-        let! reply = client.post next
+    while true do
+        let reply = sendReceive Query            
         let command = defaultArg reply Timedout
         
         match command with
         | Timedout ->
-            return! loop Query
+            ()
         | Noop | Ack -> 
-            do! Async.Sleep 100
-            return! loop Query
+            Thread.Sleep 100
+            ()
         | Exit -> 
-            return ()
+            exit 0
         | command ->
             printfn "Recv"
-            subject.OnNext command
-            return! loop Query
-    }
+            subject.OnNext command    
 
-    printfn "Server started for %s" file
-    loop Query |> Async.RunSynchronously
     0 // return an integer exit code
