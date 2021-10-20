a7bd9c121b3a703b625c6ca59be8189cf56c1bd9
Asti <asti@dynz.net>
2021-10-20 18:17:30 +0530

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,2 +1,2 @@
-dotnet pack .\src\FsWorksheet.fsproj -c Release -o nupkg
-dotnet tool install --add-source .\nupkg -g FSharp.Worksheet
+dotnet pack ./src/FsWorksheet.fsproj -c Release -o nupkg
+dotnet tool install --add-source ./nupkg -g FSharp.Worksheet
